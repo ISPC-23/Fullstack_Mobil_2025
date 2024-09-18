83c96c5c7dc031da501731610100149fb03950c2
Veronica <informaticavc.30@gmail.com>
2024-09-17 22:57:04 -0300

    agrego código

--- app/src/main/java/com/example/tiendafull/UI/UI/DetailActivity.java
+++ app/src/main/java/com/example/tiendafull/UI/UI/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.tiendafull.UI.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -31,5 +33,52 @@ public class DetailActivity extends AppCompatActivity {
 
     public void retornar(View v){
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menudeopciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem
+                                                 item) {
+        int nro = item.getItemId();
+        if (nro == R.id.entrar) {
+            startActivity(new Intent(this, LoginActivity.class)); // Assuming LoginActivity exists
+            return true;
+        } else if (nro == R.id.productos) {
+            // Already in MainActivity, no need for intent
+            return true;
+        } else if (nro == R.id.contacto) {
+            startActivity(new Intent(this, ContactActivity.class)); // Assuming ContactActivity exists
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuItem loginItem = menu.findItem(R.id.entrar);
+        MenuItem productoItem = menu.findItem(R.id.productos);
+        MenuItem contactoItem = menu.findItem(R.id.contacto);
+
+        // Hago visible todas las opciones
+        loginItem.setVisible(true);
+        productoItem.setVisible(true);
+        contactoItem.setVisible(true);
+        // hago bno visible la opcion donde me encuentro parado
+        if (this.getClass().equals(LoginActivity.class)) {
+            loginItem.setVisible(false);
+        }
+        if (this.getClass().equals(MainActivity.class)) {
+            productoItem.setVisible(false);
+        }
+        if (this.getClass().equals(ContactActivity.class)) {
+            contactoItem.setVisible(false);
+        }
+        return true;
     }
 }
