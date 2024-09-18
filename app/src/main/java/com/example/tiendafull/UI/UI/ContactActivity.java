49082f350e9f41f8d2f02765d50b1a2517eb1eb1
Veronica <informaticavc.30@gmail.com>
2024-09-17 21:53:12 -0300

    corrijo código

--- app/src/main/java/com/example/tiendafull/UI/UI/ContactActivity.java
+++ app/src/main/java/com/example/tiendafull/UI/UI/ContactActivity.java
@@ -114,11 +114,9 @@ public class ContactActivity extends AppCompatActivity {
         MenuItem productoItem = menu.findItem(R.id.productos);
         MenuItem contactoItem = menu.findItem(R.id.contacto);
 
-        // Hago visible todas las opciones
         loginItem.setVisible(true);
         productoItem.setVisible(true);
         contactoItem.setVisible(true);
-        // hago bno visible la opcion donde me encuentro parado
         if (this.getClass().equals(LoginActivity.class)) {
             loginItem.setVisible(false);
         }
