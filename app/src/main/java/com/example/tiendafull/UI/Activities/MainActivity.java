495e279274fd753be0becae1c13bb5ddc317872e
Fede Krenn <fedekrenn@gmail.com>
2024-10-07 21:01:22 -0300

    Se crean fragments y se edita el Main Activity

--- app/src/main/java/com/example/tiendafull/UI/Activities/MainActivity.java
+++ app/src/main/java/com/example/tiendafull/UI/Activities/MainActivity.java
@@ -26,81 +26,19 @@ import com.example.tiendafull.UI.ViewModels.ProductViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerView;
-    private AdaptadorProducto adaptadorProducto;
-    private ArrayList<Products> listaproducts=new ArrayList<>();
-    private ProductViewModel productViewModel;
-
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return
-                    insets;
+            return insets;
         });
+        getSupportFragmentManager().beginTransaction().add(R.id.frame2,new ProductFragment() ).commit();
 
-
-        recyclerView = findViewById(R.id.rv1);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
-        SessionManager sessionManager = SessionManager.getInstance(this);
-        productViewModel.setSessionManager(sessionManager);
-
-        adaptadorProducto = new AdaptadorProducto(this.listaproducts, this);
-        recyclerView.setAdapter(adaptadorProducto);
-        productViewModel.fetchAllProducts();
-
-        productViewModel.getProductListLiveData().observe(this, new Observer<List<Products>>() {
-            @Override
-            public void onChanged(List<Products> products) {
-
-                if (products != null) {
-
-                    adaptadorProducto.updateProductList(products);
-
-                }
-            }
-
-        });
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menudeopciones, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem
-                                                 item) {
-        int nro = item.getItemId();
-        if (nro == R.id.entrar) {
-            startActivity(new Intent(this, LoginActivity.class)); // Assuming LoginActivity exists
-            return true;
-        } else if (nro == R.id.productos) {
-            // Already in MainActivity, no need for intent
-            return true;
-        } else if (nro == R.id.contacto) {
-            startActivity(new Intent(this, ContactActivity.class)); // Assuming ContactActivity exists
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void irAlDetalle(View v) {
-        Intent intento = new Intent(this, DetailActivity.class);
-        startActivity(intento);
     }
 }
