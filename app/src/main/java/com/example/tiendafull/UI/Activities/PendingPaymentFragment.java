0800109ca5374450a72f2c9a9d537293f3c3f060
Mateo88XD <mateobeltramone2000@gmail.com>
2025-06-02 17:47:20 -0300

    Finalizado diseño de pending y rejected

---
+++ app/src/main/java/com/example/tiendafull/UI/Activities/PendingPaymentFragment.java
@@ -0,0 +1,22 @@
+package com.example.tiendafull.UI.Activities;
+
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.tiendafull.R;
+
+public class PendingPaymentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pending_payment, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/tiendafull/UI/Activities/RejectedPaymentFragment.java
@@ -0,0 +1,20 @@
+package com.example.tiendafull.UI.Activities;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.tiendafull.R;
+
+public class RejectedPaymentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rejected_payment, container, false);
+    }
+}
